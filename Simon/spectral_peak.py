9004c366fab7f6c304195742399b431d0f14815e
bryceamacker <bryceamacker@gmail.com>
2014-04-26 20:46:01 -0500

    Got spectral_peak.py to correctly id 5 sounds recorded in red blue ... .wav

--- Simon/spectral_peak.py
+++ Simon/spectral_peak.py
@@ -1,43 +1,54 @@
-from numpy import sin, linspace, pi, maximum
-from pylab import plot, show, title, xlabel, ylabel, subplot, xlim
-from scipy import fft, arange
-from scipy.io.wavfile import read
+import numpy
+from scipy import arange, fft
+from scipy.io.wavfile import read, write
 
-def plotSpectrum(y,Fs):
- """
- Plots a Single-Sided Amplitude Spectrum of y(t)
- """
- n = len(y) # length of the signal
- k = arange(n)
- T = float(n)/float(Fs)
- frq = k/T # two sides frequency range line 13 zero division
- frq = frq[range(n/2)] # one side frequency range
+def detectColor(y, rate):
+	"""
+	Plots a Single-Sided Amplitude Spectrum of y(t)
+	"""
+	Fs = rate;  # sampling rate
+	#Ts = 1.0 / Fs; # sampling interval
+	#t = arange(0,1, 1.0 / len(data)) # time vector
 
- Y = fft(y)/n # fft computing and normalization
- Y = Y[range(n/2)]
- #xlim([0, 2000])
- max_y = max(abs(Y))
- max_x = frq[abs(Y).argmax()]
- print(max_x)
- plot(frq,abs(Y),'r') # plotting the spectrum
- xlabel('Freq (Hz)')
- ylabel('|Y(freq)|')
- 
-rate, data = read('Blue mono.wav')
-ff = 5;   # frequency of the signal
+	n = len(y) # length of the signal
+	k = arange(n)
+	T = float(n)/float(Fs)
+	frq = k/T # two sides frequency range line 13 zero division
+	frq = frq[range(n/2)] # one side frequency range
 
+	Y = fft(y)/n # fft computing and normalization
+	Y = Y[range(n/2)]
+	max_y = max(abs(Y))
+	max_x = frq[abs(Y).argmax()]
+	print(max_x)
+	color = ""
+	if (1074 - 60 <= max_x <= 1074 + 60):
+		color = "Blue"
+	if (967 - 60 <= max_x <= 967 + 60):
+		color = "Red"
+	if (1257 - 60 <= max_x <= 1257 + 60):
+		color = "Green"
+	if (777 - 60 <= max_x <= 777 + 60):
+		color = "Yellow"
 
-Fs = rate;  # sampling rate
-Ts = 1.0/Fs; # sampling interval
-t = arange(0,1,1.0/len(data)) # time vector
-t_y = arange(0, 1, (1.0/150.0))
-y = sin(2*pi*ff*t_y)
-subplot(2,1,1)
-plot(t,data)
-xlabel('Time')
-ylabel('Amplitude')
+	return color
 
-# THE GOOD PART
-subplot(2,1,2)
-plotSpectrum(data,Fs)
-show()
+if __name__ == '__main__':
+	rate, data = read("red blue yellow green red.wav")
+	numOfTones = 5
+	toneLengthSec = .232 # Approximate length of each tone
+	toneLengthIndex = float(rate) * toneLengthSec
+	timeBetweenTones = .165
+	skipToNextTone = float(rate) * timeBetweenTones
+	length = float(len(data)) / float(rate) # length of file
+	# Take the amount of time it takes for the first tone (.293 s)
+	# and slice the data array to start at that time (the beginnig of the red tone)
+	beginningIndex = float(rate) * .293 
+	data = data[beginningIndex:]
+	toneList = []
+	i = 0
+	toneFile = []
+	for tones in range(numOfTones):
+		toneList.append(data[:toneLengthIndex])
+		data = data[toneLengthIndex + skipToNextTone + i*.005:]
+		print(detectColor(toneList[tones], rate))
