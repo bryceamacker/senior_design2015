37ed0b71bb67f243679f287795634add584a3ab4
bryceamacker <bryceamacker@gmail.com>
2014-09-21 20:37:46 -0500

    Arduino code to bring down platform and turn cube!

---
+++ Rubiks/CubeTurn/CubeTurn.ino
@@ -0,0 +1,33 @@
+// Sweep
+// by BARRAGAN <http://barraganstudio.com> 
+// This example code is in the public domain.
+
+
+#include <Servo.h> 
+ 
+ Servo platform;
+Servo myservo;  // create servo object to control a servo 
+                // a maximum of eight servo objects can be created 
+ 
+int pos = 0;    // variable to store the servo position 
+ 
+void setup() 
+{ 
+  platform.attach(10);
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+  platform.write(180);
+  delay(2000);
+  myservo.write(0);
+  platform.write(90 - 30 - 6);
+//  myservo.write(10);
+//  delay(2000);
+} 
+ 
+ 
+void loop() 
+{ 
+  myservo.write(0);
+  delay(2000);
+  myservo.write(170);
+  delay(2000);
+} 
